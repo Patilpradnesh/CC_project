5cf091b2b7066509932a6762e350bee5bdabd970
Pradnesh Patil <122784285+Patilpradnesh@users.noreply.github.com>
2025-02-11 09:59:56 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 num=int(input("Enter the number for "))
 if num >1
   for i in range(2,num)
-    if num % i ==0;
+    if num % i ==0
         print("number is even")
-else:
-  print("number is odd")
+    else:
+        print("number is odd")
